5f1601da8ee19c527b4ff21334185009ffa1eb10
DannisWu <airhello@qq.com>
2016-05-14 07:19:10 +0800

    add button and editText

--- android/app/src/main/java/com/neu/wudan/multicast_demo/MainActivity.java
+++ android/app/src/main/java/com/neu/wudan/multicast_demo/MainActivity.java
@@ -1,13 +1,19 @@
 package com.neu.wudan.multicast_demo;
 
 import android.content.Context;
+import android.inputmethodservice.ExtractEditText;
 import android.net.wifi.WifiManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity
 {
     private static String TAG = MainActivity.class.getSimpleName();
+    private Button mConnect = null;
 
     WifiManager.MulticastLock mMulticastLock;
     WifiManager mWifiManager;
@@ -20,9 +26,21 @@ public class MainActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mConnect = (Button)findViewById(R.id.connect);
+        //mConnect.setOnClickListener(new MyButtonListener());
+
         mWifiManager = (WifiManager)getSystemService(Context.WIFI_SERVICE);
         mMulticastLock = mWifiManager.createMulticastLock("MulticastClent");
         mMulticastLock.acquire();
+    }
+
+    public void buttonConnectClick(View view)
+    {
+        ExtractEditText mMultiServerText = (ExtractEditText)findViewById(R.id.editText_server);
+        mMulticastServer = mMultiServerText.getText().toString();
+
+        ExtractEditText mMultiPortText = (ExtractEditText)findViewById(R.id.editText_port);
+        mMulticastPort = Integer.parseInt(mMultiPortText.getText().toString());
 
         mMulticastClient = new MulticastClient(mMulticastServer, mMulticastPort);
     }
